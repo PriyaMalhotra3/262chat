d11331e117c48f6385fa0aedef6bc0d582ab034c
Priya Malhotra <priyamalhotra@college.harvard.edu>
2023-02-13 22:52:26 -0500

    More GUI

--- gui.py
+++ gui.py
@@ -1,84 +1,180 @@
+from tkinter import TclError, messagebox
 from customtkinter import *
 
 from part1.client import *
+
+set_appearance_mode("dark")
 
 class Chat(CTkToplevel):
     def __init__(self, session: Session):
         super().__init__()
 
-        
-
 class App(CTk):
     def __init__(self):
         super().__init__()
+        self.destroyed = False
 
         self.geometry("400x300")
         self.title("262chat")
 
         CTkLabel(
-            self,
-            "Welcome to 262chat!",
+            master=self,
+            text="Welcome to 262chat!",
             font=CTkFont(
                 weight="bold",
-                size=100
+                size=48
+            )
+        ).pack(pady=(32, 0))
+        CTkLabel(
+            master=self,
+            text="by Priya Malhotra and Lawrence Bjerkestrand",
+            font=CTkFont(
+                slant="italic",
+                size=14
             )
         ).pack()
+
+        server_frame = CTkFrame(master=self)
+        server_frame.pack(
+            fill=X,
+            padx=32,
+            pady=20,
+            ipadx=10,
+            ipady=10
+        )
         CTkLabel(
-            self,
-            "by Priya Malhotra and Lawrence Bjerkestrand",
-            font=CTkFont(slant="italic")
-        ).pack()
-
-        server_frame = CTkFrame(self)
-        server_frame.pack(fill=X)
-        CTkLabel(server_frame, "Server: ").pack(side=LEFT)
-        host = CTkEntry(
-            server_frame,
+            master=server_frame,
+            text="Server:"
+        ).pack(
+            side=LEFT,
+            padx=(16, 0)
+        )
+        self.host = CTkEntry(
+            master=server_frame,
             placeholder_text="Host"
-        ).pack(side=LEFT, fill=BOTH, expand=True)
-        port = CTkEntry(
-            server_frame,
+        )
+        self.host.pack(
+            side=LEFT,
+            fill=X,
+            expand=True,
+            padx=10
+        )
+        self.port = CTkEntry(
+            master=server_frame,
             placeholder_text="Port"
-        ).pack(side=LEFT, fill=BOTH, expand=True)
+        )
+        self.port.pack(
+            side=LEFT,
+            fill=X,
+            expand=True,
+            padx=10
+        )
 
-        tabview = CTkTabview(self)
-        tabview.pack(fill=BOTH, expand=True)
-        for tabname in ["Register", "Login"]:
-            tab = tabview.add(tabname)
+        username = StringVar()
+        password = StringVar()
+        self.tabview = CTkTabview(master=self)
+        self.tabview.pack(
+            fill=BOTH,
+            expand=True,
+            padx=32,
+            pady=(0,48),
+        )
+        for tab in [self.tabview.add("Register"),
+                    self.tabview.add("Login")]:
             tab.grid_columnconfigure(1, weight=1)
 
-            CTkLabel(tab, "Username: ").grid(row=0,column=0)
-            username = CTkEntry(tab, textvariable=username)
-            username.grid(row=0, column=1)
-            CTkLabel(tab, "Password: ").grid(row=1,column=0)
-            password = CTkEntry(tab, textvariable=password, show="•")
-            password.grid(row=0, column=1)
-            button = CTkButton(tab, "Connect").grid(row=0, column=0, columnspan=2)
+            CTkLabel(
+                master=tab,
+                text="Username:"
+            ).grid(
+                row=0,
+                column=0,
+                sticky=W,
+                padx=10
+            )
+            CTkEntry(
+                master=tab,
+                textvariable=username
+            ).grid(
+                row=0,
+                column=1,
+                sticky=EW,
+                pady=10
+            )
+            CTkLabel(
+                master=tab,
+                text="Password:"
+            ).grid(
+                row=1,
+                column=0,
+                sticky=W,
+                padx=10
+            )
+            CTkEntry(
+                master=tab,
+                show="•",
+                textvariable=password
+            ).grid(
+                row=1,
+                column=1,
+                sticky=EW,
+                pady=10
+            )
 
-            async def connect():
-                button.configure(state="disabled")
-                tabview.configure(state="disabled")
-                try:
-                    session = await Session.connect(host.get(), int(port.get()))
-                    if tabname == "Register":
-                        endpoint = session.register
-                    else:
-                        endpoint = session.login
-                    await endpoint(username.get(), password.get())
-                    self.withdraw()
-                    Chat(session)
-                except (ValueError, ChatException) as e:
-                    messagedialog.showerror("Error", e.args[0])
-                    tabview.configure(state="enabled")
-                    button.configure(state="enabled")
-            button.configure(command=lambda: asyncio.create_task(connect()))
+            button = CTkButton(
+                master=tab,
+                text="Connect"
+            )
+            button.grid(
+                row=2,
+                column=0,
+                columnspan=2,
+                sticky=NSEW,
+                padx=10,
+                pady=10,
+                ipadx=5,
+                ipady=5
+            )
+            button.configure(command=lambda source=button: asyncio.create_task(self.connect(source)))
+
+    async def connect(self, source):
+        source.configure(state="disabled")
+        self.tabview.configure(state="disabled")
+        try:
+            try:
+                port = int(self.port.get())
+            except ValueError:
+                raise ValueError("Port must be an integer.")
+            session = await Session.connect(self.host.get(), port)
+            if self.tabview.get() == "Register":
+                endpoint = session.register
+            else:
+                endpoint = session.login
+            await endpoint(username, password)
+            self.withdraw()
+            Chat(session)
+        except Exception as e:
+            print(e)
+            messagebox.showerror("Error", str(e))
+            source.configure(state="normal")
+            self.tabview.configure(state="normal")
 
     def mainloop(self):
         async def loop():
-            while True:
-                app.update()
-                await asyncio.sleep(0.1)
+            try:
+                while not self.destroyed:
+                    self.update()
+                    await asyncio.sleep(0.01)
+            except TclError:
+                pass
         asyncio.run(loop())
+
+    def destroy(self):
+        destroy_now = super().destroy
+        async def async_destroy():
+            self.destroyed = True
+            destroy_now()
+        asyncio.create_task(async_destroy())
 
 app = App()
 app.mainloop()
